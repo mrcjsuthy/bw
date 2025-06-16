cadf23ae915d3bcce8c67db39a722c7065830149
GreenAisle <141960866+GreenAisle@users.noreply.github.com>
2025-06-16 22:47:51 +1200

    Remove MUI Core resources

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { Container, Typography, Button, Box } from '@mui/material';
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Welcome to MUI Sample Page
-        </Typography>
-        <Typography variant="body1" paragraph>
-          This is a sample page using Material-UI components.
-        </Typography>
-        <Button variant="contained" color="primary">
-          Click Me
-        </Button>
-      </Box>
-    </Container>
+    <div>
+      <h1>Welcome</h1>
+    </div>
   );
 }
 
